85847581f947526fa53fc9b69c65615006757574
nada <nazaeissa@gmail.com>
2023-12-20 15:40:29 +0200

    lk

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,15 +25,15 @@ void close_elf(int elf);
  */
 void check_elf(unsigned char *e_ident)
 {
-	int index;
+        int index;
 
-	for (index = 0; index < 4; index++)
-	{
-		if (e_ident[index] != 127 &&
-		    e_ident[index] != 'E' &&
-		    e_ident[index] != 'L' &&
-		    e_ident[index] != 'F')
-		{
+        for (index = 0; index < 4; index++)
+        {
+                if (e_ident[index] != 127 &&
+                    e_ident[index] != 'E' &&
+                    e_ident[index] != 'L' &&
+                    e_ident[index] != 'F')
+			{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
 		}
